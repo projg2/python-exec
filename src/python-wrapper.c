87e741baaf91f2f1e660c1b067e976ab3779ebf4
Michał Górny <mgorny@gentoo.org>
2012-10-05 18:47:23 +0200

    Initiate python-wrapper.c.

---
+++ src/python-wrapper.c
@@ -0,0 +1,13 @@
+/* python-wrapper-r1
+ * (c) 2012 Michał Górny
+ * Licensed under the terms of the 2-clause BSD license.
+ */
+
+#ifdef HAVE_CONFIG_H
+#	include "config.h"
+#endif
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
