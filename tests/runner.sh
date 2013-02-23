b3c26a58f8eff40d03b2353aae90ddbc004d745b
Michał Górny <mgorny@gentoo.org>
2013-02-23 08:49:58 +0100

    Introduce initial self-testing support.

---
+++ tests/runner.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [ ${#} -ne 2 ]; then
+	echo "Usage: ${0} <python-impls> <test-script>"
+	exit 1
+fi
+
+# automake codes
+SKIP=77
+ERROR=99
+
+# common metadata
+PYTHON_IMPLS=${1}
+TEST=${2}
+TEST_NAME=${2##*/}
+TEST_TMP=tests/${TEST_NAME}.tmp
+
+# helper functions
+write_impl() {
+	echo "${2}" > "${TEST_TMP}-${1}"
+	chmod -w,+x "${TEST_TMP}-${1}"
+}
+
+do_exit() {
+	trap - EXIT
+	exit "${@}"
+}
+
+do_test() {
+	set +e
+
+	"${@}"
+	ret=${?}
+	echo "Test result: ${ret}" >&2
+	do_exit ${ret}
+}
+
+# catch all failures
+trap 'exit 99' EXIT
+set -e
+
+rm -f "${TEST_TMP}"*
+ln -s python-exec "${TEST_TMP}"
+. "${TEST}"
